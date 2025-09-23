b1489b3cc47c5095498b8d2b756c08686ce2aeec
LonelyThic <181782450+LonelyThic@users.noreply.github.com>
2025-09-23 14:24:24 -0700

    Update pizzaSwap.c

--- Lab11/pizzaSwap.c
+++ Lab11/pizzaSwap.c
@@ -3,18 +3,18 @@
 void swap(int *a, int *b);
 
 int main() {
-    int X = 9;
-    int Y = 4;
+    int pizzasCustomer1 = 9;
+    int pizzasCustomer2 = 4;
 
     printf("Before swap:\n");
-    printf("Customer 1 has %d pizzas\n", X);
-    printf("Customer 2 has %d pizzas\n", Y);
+    printf("Customer 1 (John) has %d pizzas\n", pizzasCustomer1);
+    printf("Customer 2 (Mary) has %d pizzas\n", pizzasCustomer2);
 
-    swap(&X, &Y);
+    swap(&pizzasCustomer1, &pizzasCustomer2);
 
     printf("\nAfter swap:\n");
-    printf("Customer 1 has %d pizzas\n", X);
-    printf("Customer 2 has %d pizzas\n", Y);
+    printf("Customer 1 (John) has %d pizzas\n", pizzasCustomer1);
+    printf("Customer 2 (Mary) has %d pizzas\n", pizzasCustomer2);
 
     return 0;
 }
