a8c9ab4c513ddcd2efb8f370dfd500ef52036139
LonelyThic <181782450+LonelyThic@users.noreply.github.com>
2025-09-23 13:59:59 -0700

    Starting Lab11

---
+++ Lab11/pizzaSwap.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void swap(int a, intb);
+
+int main() {
+    int X = 9;
+    int Y = 4;
+
+    printf("Before swap:\n");
+    printf("Customer 1 has %d pizzas\n", X);
+    printf("Customer 2 has %d pizzas\n", Y);
+
+    swap(&X, &Y);
+
+    printf("\nAfter swap:\n");
+    printf("Customer 1 has %d pizzas\n", X);
+    printf("Customer 2 has %d pizzas\n", Y);
+
+    return 0;
+}
+
+void swap(int a, intb) {
+    int temp = *a;
+    a =b;
+    *b = temp;
+}
