b3308d8062527f19b4ebfe110fe5789af78dc8e5
LonelyThic <181782450+LonelyThic@users.noreply.github.com>
2025-09-30 11:01:57 -0700

    finishing homework 2

---
+++ Hw2/library_books.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TITLE_LEN 100
+#define AUTHOR_LEN 100
+
+typedef struct {
+    char title[TITLE_LEN];
+    char author[AUTHOR_LEN];
+    int year;
+} Book;
+
+int main() {
+    Book *library = NULL;
+    int count = 0;
+    int choice;
+
+    while (1) {
+        printf("\nLibrary Menu:\n");
+        printf("1. Add New Book\n");
+        printf("2. View All Books\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        getchar();
+
+        if (choice == 1) {
+            Book *temp = realloc(library, (count + 1) * sizeof(Book));
+            if (!temp) {
+                printf("Memory allocation failed.\n");
+                break;
+            }
+            library = temp;
+
+            printf("Enter book title: ");
+            fgets(library[count].title, TITLE_LEN, stdin);
+            library[count].title[strcspn(library[count].title, "\n")] = '\0';
+
+            printf("Enter author name: ");
+            fgets(library[count].author, AUTHOR_LEN, stdin);
+            library[count].author[strcspn(library[count].author, "\n")] = '\0';
+
+            printf("Enter year published: ");
+            scanf("%d", &library[count].year);
+            getchar();
+
+            count++;
+            printf("Book added successfully!\n");
+
+        } else if (choice == 2) {
+            if (count == 0) {
+                printf("No books in the system.\n");
+            } else {
+                printf("\n--- Book List ---\n");
+                for (int i = 0; i < count; i++) {
+                    printf("%d. \"%s\" by %s (%d)\n",
+                           i + 1,
+                           library[i].title,
+                           library[i].author,
+                           library[i].year);
+                }
+            }
+
+        } else if (choice == 3) {
+            printf("Exiting program. Goodbye!\n");
+            break;
+        } else {
+            printf("Invalid choice. Please try again.\n");
+        }
+    }
+
+    free(library);
+    return 0;
+}
---
+++ Hw2/workdays.c
@@ -0,0 +1,38 @@
+/*
+ * CS 3035 Homework 2 - Q2
+ * File: workdays.c
+ * Description:
+ *   Prompts user for a day of the week (0–6) and
+ *   tells whether it is a workday or weekend.
+ *   Uses enumeration and switch case without magic numbers.
+ */
+
+#include <stdio.h>
+
+int main() {
+    enum daysOfWeek { MON = 0, TUES, WED, THURS, FRI, SAT, SUN };
+
+    int input;
+    printf("Enter a day of the week (0=Mon, 1=Tue, ..., 6=Sun): ");
+    scanf("%d", &input);
+
+    enum daysOfWeek day = (enum daysOfWeek) input;
+
+    switch (day) {
+        case MON:
+        case TUES:
+        case WED:
+        case THURS:
+        case FRI:
+            printf("It is a WORKDAY.\n");
+            break;
+        case SAT:
+        case SUN:
+            printf("It is a WEEKEND.\n");
+            break;
+        default:
+            printf("Invalid input. Please enter a number 0–6.\n");
+            break;
+    }
+    return 0;
+}
