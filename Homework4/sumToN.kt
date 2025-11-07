f5986897fa068a3086767e0b0aa42f633bb19249
LonelyThic <LonelyThic@users.noreply.github.com>
2025-11-07 09:48:40 -0800

    Update sumToN.kt

--- Homework4/sumToN.kt
+++ Homework4/sumToN.kt
@@ -1,10 +1,8 @@
 fun sumToN(n: Int): Int {
     if (n == 1) return 1
-
     return n + sumToN(n - 1)
 }
 
 fun main() {
-    val n = 5
-    println("Sum from 1 to $n is: ${sumToN(n)}")
+    println("Sum from 1 to 5: ${sumToN(5)}")
 }
