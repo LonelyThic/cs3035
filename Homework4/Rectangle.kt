65ae6727a2742ef97e34286c45db12d29c488c49
LonelyThic <LonelyThic@users.noreply.github.com>
2025-11-07 09:50:12 -0800

    Update Rectangle.kt

--- Homework4/Rectangle.kt
+++ Homework4/Rectangle.kt
@@ -1,18 +1,11 @@
 class Rectangle(val width: Double, val height: Double) {
 
-    fun area(): Double {
-        return width * height
-    }
+    fun area(): Double = width * height
 
-    fun isSquare(): Boolean {
-        return width == height
-    }
+    fun isSquare(): Boolean = width == height
 }
 
 fun main() {
-    val rect1 = Rectangle(4.0, 5.0)
-    val rect2 = Rectangle(6.0, 6.0)
-
-    println("Rectangle 1: area = ${rect1.area()}, isSquare = ${rect1.isSquare()}")
-    println("Rectangle 2: area = ${rect2.area()}, isSquare = ${rect2.isSquare()}")
-}
+    val rect = Rectangle(5.0, 5.0)
+    println("Area: ${rect.area()}")
+    println("Is square: ${rect.isSquare()}")
